5e188636d5907a901632f873d36b33a226859b84
Sasi Kumar Viothula <vs.sasikumar2004@gmail.com>
2026-05-30 22:39:40 +0530

    feat: add delete, clear, resize, peek, stats, keys, thread lock

--- lru_cache.py
+++ lru_cache.py
@@ -200,7 +200,7 @@ class LRUCache:
                 lru = self.tail.prev
                 self._remove(lru)
                 del self.map[lru.key]
-                self._evictions += 1
+                self._evictions += 2
  
     def peek(self, key) -> object:
         """
